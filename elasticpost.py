f764bbef748e55c921c1c3dd1363cea9ca78b366
Evan Sonderegger <evan.sonderegger@gmail.com>
2015-10-16 15:12:35 -0400

    updated post to elastic search

--- elasticpost.py
+++ elasticpost.py
@@ -1,11 +1,10 @@
 import elasticsearch
 import json
 
-with open('airdata4.json') as data_file:
+with open('flightdata.json') as data_file:
     airdata = json.load(data_file)
 
 es = elasticsearch.Elasticsearch([{'host': '159.203.93.188', 'port': 9200}])
 
 for i in range(len(airdata)):
-    # print airdata[i]['Agency']
     es.index(index='flights', doc_type='flight', id=i, body=airdata[i])
